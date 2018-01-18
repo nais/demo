f555c3106b50392d1f7bd72431ed94b42b7561e8
Giao The Cung <giao.the.cung@nav.no>
2018-01-18 10:21:11 +0100

    Revert "reset."
    This reverts commit a03d29fa00bbecca6db4d1b810de4514bda9ad0f.

--- src/main/java/nais/nav/no/demo/DemoApplication.java
+++ src/main/java/nais/nav/no/demo/DemoApplication.java
@@ -1,15 +1,18 @@
 package nais.nav.no.demo;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 
 @SpringBootApplication
 @RestController
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -20,5 +23,18 @@ public class DemoApplication {
     public String hello(@RequestParam(value="name", defaultValue = "World") String name) {
         return String.format("Hello, %s!", name);
     }
+
+    @RequestMapping(value="/isAlive",method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String isAlive() {
+        return "Alive";
+    }
+
+    @RequestMapping(value="/isReady",method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String isReady() {
+        return "Ready";
+    }
+
 
 }
