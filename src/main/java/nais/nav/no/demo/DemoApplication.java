5123bd62968d787941f39af0b87205e2b81f35fa
Giao The Cung <giaothe@gmail.com>
2018-02-23 14:48:14 +0100

    logback

--- src/main/java/nais/nav/no/demo/DemoApplication.java
+++ src/main/java/nais/nav/no/demo/DemoApplication.java
@@ -1,22 +1,15 @@
 package nais.nav.no.demo;
 
-import ch.qos.logback.classic.LoggerContext;
 import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
-import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
-import java.util.Optional;
 
 
 @SpringBootApplication
@@ -52,7 +45,7 @@ public class DemoApplication {
 
     @Scheduled(fixedRate = 5000)
     public void log() {
-        log.info(" BrukerId=\"Z990691\" ber om tilgang til action=\"create\" for fnr=\"111111111\" i domene=\"sak\". Tilgang gis med decision=\"Permit\"");
+        log.info("BrukerId=\"Z990691\" ber om tilgang til action=\"create\" for fnr=\"111111111\" i domene=\"sak\". Tilgang gis med decision=\"Permit\"");
     }
 
 
